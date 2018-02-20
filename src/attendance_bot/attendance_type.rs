e11afbdde253044d6e9c2a2e9a95fa0e8658b226
atsushi130 <atsushi_main@outlook.jp>
2018-02-20 09:24:45 +0900

    define: attendance type

--- src/attendance_bot/attendance_type.rs
+++ src/attendance_bot/attendance_type.rs
@@ -3,3 +3,13 @@ pub enum AttendanceType {
     CheckIn,
     CheckOut
 }
+
+impl AttendanceType {
+    pub fn from(value: &str) -> Self {
+        match value {
+            "出勤" => AttendanceType::CheckIn,
+            "退勤" => AttendanceType::CheckOut,
+            _     => AttendanceType::CheckIn
+        }
+    }
+}
