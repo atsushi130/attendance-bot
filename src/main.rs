8ec545c0b63966641672979ee1fb3b0a519d338c
atsushi130 <atsushi_main@outlook.jp>
2018-02-21 09:16:09 +0900

    implement: running code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 
 extern crate slack;
+use slack::RtmClient;
 
 mod attendance_bot;
+use attendance_bot::AttendanceBot;
 
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = std::env::args().collect();
+    let token = match args.len() {
+        0 | 1 => panic!("token not found."),
+        x => args[x - 1].clone(),
+    };
+
+    connect(&token);
+}
+
+fn connect(token: &str) {
+
+    let mut handler = AttendanceBot::from();
+    let r = RtmClient::login_and_run(&token, &mut handler);
+    match r {
+        Ok(_) => {}
+        Err(_) => connect(token)
+    }
 }
