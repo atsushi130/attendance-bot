d8f4b412926a1ccdba912f8500f35aba4cd4dc09
atsushi130 <atsushi_main@outlook.jp>
2018-02-22 09:10:24 +0900

    update: attendance bot module

--- src/attendance_bot/mod.rs
+++ src/attendance_bot/mod.rs
@@ -4,3 +4,6 @@ pub use self::attendance_bot::AttendanceBot;
 
 mod attendance_type;
 use self::attendance_type::AttendanceType;
+
+mod attendance_tokenizer;
+use self::attendance_tokenizer::AttendanceTokenizer;
