83f7edd5fb54c81bda5a880d66f8378f007fd5d9
atsushi130 <atsushi_main@outlook.jp>
2018-02-23 09:11:51 +0900

    define: attendance model

---
+++ src/api_client/attendance.rs
@@ -0,0 +1,16 @@
+
+pub struct Attendance {
+    user: String,
+    date: String,
+    type_string: String
+}
+
+impl Attendance {
+    pub fn from(user: &str, date: &str, type_string: &str) -> Self {
+        Attendance {
+            user: user.to_string(),
+            date: date.to_string(),
+            type_string: type_string.to_string()
+        }
+    }
+}
