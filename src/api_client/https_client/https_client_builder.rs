c96d15fc65f01b91cc809f19bbc961b4ed13bb43
atsushi130 <atsushi_main@outlook.jp>
2018-02-23 09:11:32 +0900

    add: https client builder module

---
+++ src/api_client/https_client/https_client_builder.rs
@@ -0,0 +1,15 @@
+
+use hyper::Client;
+use hyper_native_tls::NativeTlsClient;
+use hyper::net::HttpsConnector;
+
+pub struct HttpsClientBuilder;
+
+impl HttpsClientBuilder {
+    
+    pub fn build() -> Client {
+        let ssl = NativeTlsClient::new().unwrap();
+        let connector = HttpsConnector::new(ssl);
+        Client::with_connector(connector)
+    }
+}
---
+++ src/api_client/https_client/mod.rs
@@ -0,0 +1,3 @@
+
+mod https_client_builder;
+pub use self::https_client_builder::HttpsClientBuilder;
