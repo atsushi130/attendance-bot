acaefa6f87aaa87db9ec8a0e1b41b71836dd183f
atsushi130 <atsushi_main@outlook.jp>
2018-02-24 02:26:57 +0900

    implement: api request sample code

--- src/api_client/attendance_api.rs
+++ src/api_client/attendance_api.rs
@@ -1,10 +1,22 @@
 
 use super::{ Attendance, HttpsClientBuilder };
+use rustc_serialize::json;
+use std::io::Read;
 
 pub struct AttendanceApi;
 
 impl AttendanceApi {
 
     pub fn register(&self, entity: &Attendance) {
+
+        let url = "http://localhost:8000/attendances";
+        let mut response = String::new();
+
+        let json = json::encode(entity).unwrap();
+
+        HttpsClientBuilder::build()
+            .post(url)
+            .body(&*json)
+            .send();
     }
 }
