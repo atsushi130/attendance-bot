6c2a07c9fc0efef153b1f25f4890aefc0c858420
atsushi130 <atsushi_main@outlook.jp>
2018-02-24 19:21:10 +0900

    refactoring

--- src/api_client/attendance_api.rs
+++ src/api_client/attendance_api.rs
@@ -2,7 +2,6 @@
 use super::{ Attendance, HttpsClientBuilder };
 use rustc_serialize::json;
 use hyper::header::{Headers, ContentType};
-use std::io::Read;
 
 pub struct AttendanceApi;
 
@@ -11,11 +10,12 @@ impl AttendanceApi {
     pub fn register(&self, entity: &Attendance) {
 
         let url = "http://localhost:3000/attendances";
-        let json = json::encode(entity).unwrap();
         let mut headers = Headers::new();
         headers.set(ContentType::json());
 
-        HttpsClientBuilder::build()
+        let json = json::encode(entity).unwrap();
+
+        let _ = HttpsClientBuilder::build()
             .post(url)
             .headers(headers)
             .body(&*json)
