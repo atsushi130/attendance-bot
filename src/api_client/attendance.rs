ac52a4627647bb82682fed24e4f8ffee79628a1e
atsushi130 <atsushi_main@outlook.jp>
2018-02-24 19:19:14 +0900

    update: attendance model

--- src/api_client/attendance.rs
+++ src/api_client/attendance.rs
@@ -3,16 +3,16 @@
 #[derive(Debug)]
 pub struct Attendance {
     user: String,
-    date: String,
-    type_string: String
+    check_at: String,
+    attendance_type: i64
 }
 
 impl Attendance {
-    pub fn from(user: &str, date: &str, type_string: &str) -> Self {
+    pub fn from(user: &str, check_at: &str, attendance_type: &i64) -> Self {
         Attendance {
             user: user.to_string(),
-            date: date.to_string(),
-            type_string: type_string.to_string()
+            check_at: check_at.to_string(),
+            attendance_type: *attendance_type
         }
     }
 }
